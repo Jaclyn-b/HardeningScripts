aa977239266ca040d44bec2508da326b2a04fee1
Torin Sapp <31081324+torinsapp@users.noreply.github.com>
2021-08-26 10:25:21 -0400

    Update Server.bat

--- Windows/Server.bat
+++ Windows/Server.bat
@@ -1,1 +1,35 @@
+@echo off
+echo Checking if script contains big boi rights...
+net sessions
+if %errorlevel%==0 (
+echo Success!
+) else (
+echo let me do whatever I want, please run with big boi rights...
+pause
+exit
+)
+:MENU
+echo Choose An option:
+echo 1. audit machine
+echo 2. processes
+echo 3. Policies de Password
+echo 4. Find contraband
+echo 5. Disable Remote Desktop(letting bad people in our computer >:( )
+echo 6. GIve your pc the good stuff(auto-updates)
+echo 7. Disable Weak services
+echo 8. i look for evil stuff
+echo 9. rootkit go boom
 
+CHOICE /C 123456789 /M "Enter your choice:"
+if ERRORLEVEL 9 goto Nine
+if ERRORLEVEL 8 goto Eight
+if ERRORLEVEL 7 goto Seven
+if ERRORLEVEL 6 goto Six
+if ERRORLEVEL 5 goto Five
+if ERRORLEVEL 4 goto Four
+if ERRORLEVEL 3 goto Three
+if ERRORLEVEL 2 goto Two
+if ERRORLEVEL 1 goto One
+:One
+
+goto MENU
